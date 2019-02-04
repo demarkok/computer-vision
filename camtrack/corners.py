1b3f540178a8c51301355fcb7da3e94265dd5198
Plyushchenko <plyushenko@bk.ru>
2019-02-04 17:30:00 +0300

    001

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -37,13 +37,56 @@ class _CornerStorageBuilder:
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
     image_0 = frame_sequence[0]
+    max_corners = 1500
+    quality_level = 0.05
+    min_distance = 6
+    corner_coordinates = cv2.goodFeaturesToTrack(
+        image=image_0,
+        maxCorners=max_corners,
+        qualityLevel=quality_level,
+        minDistance=min_distance,
+    )
+    prev_last_id = last_id = len(corner_coordinates)
     corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+        ids=np.array(range(last_id)),
+        points=corner_coordinates,
+        sizes=np.full(last_id, min_distance)
     )
     builder.set_corners_at_frame(0, corners)
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        next_pts, status, err = cv2.calcOpticalFlowPyrLK(
+            prevImg=np.uint8(image_0 / image_0.max() * 255.0),
+            nextImg=np.uint8(image_1 / image_1.max() * 255.0),
+            prevPts=corner_coordinates,
+            nextPts=None,
+        )
+        #ids = np.argsort(err.flatten())[:int(len(err) * 0.95)]
+        ids = np.where(status == 1)[0]
+        corner_coordinates = next_pts[ids]
+        additional_corner_coordinates = []
+        if len(corner_coordinates) < max_corners:
+            potential_corner_coordinates = cv2.goodFeaturesToTrack(
+                image=image_1,
+                maxCorners=max_corners,
+                qualityLevel=quality_level,
+                minDistance=min_distance,
+            )
+            for potential_corner_coordinate in potential_corner_coordinates:
+                distances = np.linalg.norm(potential_corner_coordinate - corner_coordinates, axis=1)
+                if np.linalg.norm(distances, axis=1).min() >= min_distance:
+                    additional_corner_coordinates.append(potential_corner_coordinate)
+                    last_id += 1
+                if len(corner_coordinates) + len(additional_corner_coordinates) >= max_corners:
+                    break
+        if len(additional_corner_coordinates) != 0:
+            corner_coordinates = np.concatenate([corner_coordinates, additional_corner_coordinates])
+            ids = np.concatenate([ids, np.array(range(prev_last_id, last_id))])
+            prev_last_id = last_id
+        corners = FrameCorners(
+            ids=ids,
+            points=corner_coordinates,
+            sizes=np.full(len(corner_coordinates), min_distance),
+        )
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
 
